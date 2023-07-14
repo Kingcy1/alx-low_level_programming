b3aeb197a2e7ccc16ced9122b54403af0abed77d
Kingcy1 <azinta.acom@gmail.com>
2023-07-14 13:37:53 -0700

    Five Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Starting point (prints all single digit numbers of base 10 starting from 0)
+ * main - Starting point (all single digit numbers of base 10 starting from 0)
  * Return: 0 always
  */
 int main(void)
